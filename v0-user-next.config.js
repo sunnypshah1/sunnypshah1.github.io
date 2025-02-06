a8f71fe76db5f205b05770020211bd819006716d
sunnypshah1 <62190316+sunnypshah1@users.noreply.github.com>
2025-02-05 23:21:25 -0500

    deleting stuff

--- v0-user-next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "",
-}
-
-module.exports = nextConfig
-
