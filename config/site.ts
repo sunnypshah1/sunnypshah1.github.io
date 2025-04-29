9100784d0ecffdbe7a3f296f85bee7d441fa130a
Sunny Shah <62190316+sunnypshah1@users.noreply.github.com>
2025-04-29 15:59:51 -0400

    changes
    changes

---
+++ config/site.ts
@@ -0,0 +1,57 @@
+export const siteConfig = {
+    name: "Sunny Shah",
+    title: "Software Engineer",
+    email: "sunnysha@umich.edu",
+    phone: "734-272-5306",
+    links: {
+      github: "https://github.com/sunnypshah1",
+      linkedin: "https://linkedin.com/in/sunnypshah",
+      portfolio: "https://sunnypshah1.github.io",
+    },
+    nav: [
+      {
+        title: "Education",
+        href: "/education",
+      },
+      {
+        title: "Experience",
+        href: "/experience",
+      },
+      {
+        title: "Projects",
+        href: "/projects",
+      },
+      {
+        title: "Technical Skills",
+        href: "/skills",
+      },
+      {
+        title: "Contact",
+        href: "/contact",
+      },
+    ],
+    skills: {
+      languages: ["Python", "C++", "SQL", "JavaScript", "HTML/CSS", "LaTeX"],
+      frameworks: ["React", "Bootstrap", "jQuery", "PyTorch", "OpenCV", "Agile", "Jira"],
+      tools: ["Google Cloud Platform (GCP)", "Amazon Web Services (AWS)", "Git", "VS Code", "Jupyter Notebook", "Linux"],
+      libraries: ["pandas", "NumPy", "Matplotlib", "PyTorch", "SciPy", "Scikit-learn"],
+    },
+    education: {
+      university: "University of Michigan",
+      degree: "Bachelor of Science in Computer Science and Pure Mathematics",
+      graduation: "Expected: May 2027",
+      location: "Ann Arbor, MI",
+      courses: [
+        "Data Structures and Algorithms",
+        "Computer Science Theory",
+        "Discrete Math",
+        "Machine Learning",
+        "Computer Organization",
+        "Probability Theory",
+        "Object-Oriented Design",
+        "Operating Systems",
+        "Networking"
+      ],
+    },
+  }
+  
